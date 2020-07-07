534f0784c30cbea69a014c5837fc51442b35e9a2
Alandis Seals <alandis.seals@yahoo.com>
2020-07-07 16:20:50 -0500

    first commit

---
+++ jquery.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+    // Creating a buuton 
+    let myButton = $("<button></buttom>").text("click me");
+    $("body").append(myButton);
+
+    //Adding a click function to the button
+    $(myButton).click(function() {
+        alert('You Look Awesome !')
+    });
+
+    // Alerting the input
+    $("#Btn").click(function(e) {
+        e.preventDefault();
+        alert($("input[name= 'name']").val());
+
+
+    });
+
+    //Styling The Div
+    $("div").css({ "background-color": "blue" })
+    $(myButton).css ({"text-align": "center"})
+
+
+});
