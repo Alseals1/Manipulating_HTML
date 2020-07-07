311f1df5f4fa7a13f0f3bba75a787b638b51ba0a
Alandis Seals <alandis.seals@yahoo.com>
2020-07-07 16:32:52 -0500

    made comments

--- jquery.js
+++ jquery.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    // Creating a buuton 
-    let myButton = $("<button></buttom>").text("click me");
-    $("body").append(myButton);
+//     // Creating a buuton 
+//     let myButton = $("<button></buttom>").text("click me");
+//     $("body").append(myButton);
 
-    //Adding a click function to the button
-    $(myButton).click(function() {
-        alert('You Look Awesome !')
-    });
+//     //Adding a click function to the button
+//     $(myButton).click(function() {
+//         alert('You Look Awesome !')
+//     });
 
-    // Alerting the input
-    $("#Btn").click(function(e) {
-        e.preventDefault();
-        alert($("input[name= 'name']").val());
-
-
-    });
-
-    //Styling The Div
-    $("div").css({ "background-color": "blue" })
+//     // Alerting the input
+//     $("#Btn").click(function(e) {
+//         e.preventDefault();
+//         alert($("input[name= 'name']").val());
 
 
+//     });
 
-});
+//     //Styling The Div
+//     $("div").css({ "background-color": "blue" })
+
+
+
+// });
